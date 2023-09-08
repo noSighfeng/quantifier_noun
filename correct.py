03eef3e64fc1916fd6bd31f1f9e673c68b33e095
noSighfeng <lzyf1019@qq.com>
2023-09-08 17:16:23 +0800

    逻辑完善

--- correct.py
+++ correct.py
@@ -13,33 +13,26 @@ class correct:
     
     def init_pattern(self):
         # 数字 量词 名词
-        self.pattern = ('([\d{}]+)' + '({})' + '(?:.*?)' + '([{}]+)').format(''.join(self.num_list)
-                                                       ,'|'.join(self.quantifier_list)
-                                                       ,''.join(self.noun_list))
-        # self.pattern = ('([{}]+)' + '(.*)' + '(年)').format(''.join(self.num_list))
-        # '([]+|[]+)'
+        self.pattern = ('([\d{}]+)' + '({})' + '(?:.*?)').format(''.join(self.num_list)
+                                                       ,'|'.join(self.quantifier_list))
     def find(self,sentence):
         res = []
         self.init_pattern()
-        # print(re.findall(self.pattern,sentence))
+        # 分词后根据定义的字典提取可能的名词
+        words = [word for word,flag in posseg.lcut(sentence) if flag in ['n']]
+        # 查找这些名词所搭配的[数词量词名词]
+        self.pattern = self.pattern + '([{}]+)'.format(''.join(words))
         return re.finditer(self.pattern,sentence)
 
     def correct(self,sentence:str): # [('一', '篇', '话')]
+        if sentence ==' ' or len(sentence)==0: return 
         mybe = self.find(sentence)
         # if len(mybe) == 0 : print('没找到数词量词名词搭配') # 没找到数词量词名词搭配
+        errors_span_rights = [] # [(error,index,right)] # 错误列表error[('一', '篇', '话')]  错误范围span  过错后的词right
+        sentence_right = '' # 改错后的句子
         for i in mybe:
-            # print(i.group(2))
-            # print(i.span(2))
             span = i.span()
-            # print(span)
             i = list(i.groups())
-            # 分词判断量词所搭配名词
-            pos = [ n for n,flag in posseg.lcut(''.join(i[2])) if flag in ['n']]
-            if len(pos) ==0:# 不是名词
-                print('[{}]不是名词'.format(i[2]))
-                self.correct(sentence.replace(i[2],''))
-                continue
-            i[2] = pos[0]
             res = []
             try:
                 if i[2] not in self.quantifier_noun_dict[i[1]]: # 如果名词不在所搭配的量词词典中
@@ -47,10 +40,13 @@ class correct:
                     for k,v in self.quantifier_noun_dict.items(): # 遍历词典找到名词词可搭配的量词
                         if i[2] in v:
                             res.append(k)
-                    print(i[0] + '[%s]' % '|'.join(res) + i[2],end='\t') 
-                    print(span )
+                    #print(i[0] + '[%s]' % '|'.join(res) + i[2],end='\t') 
+                    errors_span_rights.append((i,span,(i[0] , '[%s]' % '|'.join(res) , i[2])))
+                    #print(span)
+                    #print('正确句子：{}'.format(sentence.replace(''.join(i),i[0] + '[%s]' % '|'.join(res) + i[2])))
                 else: 
-                    print('暂无错误 {}'.format(i))
+                    # print('暂无错误 {}'.format(i))
+                    print
             except Exception:
             # 存在非量词
                 print('['+''.join(i) + ']句子中的[' + i[1] + ']不是量词')
@@ -58,6 +54,7 @@ class correct:
                         if i[2] in v:
                             res.append(k)
                 print(i[0] + '[%s]' % '|'.join(res) + i[2]) 
+        print(errors_span_rights)
 
 quantifier_list = [] # 量词列表
 num_list = [] 
@@ -101,10 +98,7 @@ def init_list():
                 quantifier_noun_dict[quan] = set()
             quantifier_noun_dict[quan].update(line_spl[1:])
 
-    # 名词列表写入jieba自定义词典
-    with open('data_txt/jieba_dict_custom.txt','w',encoding='utf-8') as f:
-        for i in noun_list:
-            f.write(i + ' 10 ' + 'n' + '\n')
+    
     
 
     # 初始化自定义词典
@@ -124,6 +118,11 @@ def init_list():
             quantifier_noun_dict[quan].add(line_spl[1])
 
     
+    # 名词列表写入jieba自定义词典
+    with open('data_txt/jieba_dict_custom.txt','w',encoding='utf-8') as f:
+        for i in noun_list:
+            f.write(i + ' 10 ' + 'n' + '\n')
+
     # 纠正错误词典 错误类型：量词错误搭配
     dict_error_path = os.path.join(os.getcwd(),'data_txt/dict_error.txt')
     with open(dict_error_path,'r',encoding='utf-8') as f:
@@ -137,12 +136,15 @@ if __name__ == '__main__':
     init_list()
     co = correct(noun_list,quantifier_list,num_list,quantifier_noun_dict)
     text = [
-        '这篇文章的每一句话表达了作者这三种月以来的颠沛流离',
+        '这篇文章的每一则话表达了作者这三种月以来的颠沛流离',
         '表达了作者一个强烈的情感',
-        '这里有好多个苹果',
-        '这将是一个重要的时刻',
-        '一份情感',
-        '一个无与伦比'
+        '一项情感',
+        '一各漂亮的苹果',
+        '每到中秋节，家家户户都会吃一轮圆圆的月饼',
+        '我的衣柜里有六件衣服，四件裤子和两件帽子',
+        '墙上挂着一副画',
+        '一对眼镜',
+        '她发现自己头上有一条白头发'
     ]
     for i in text:
         co.correct(i)
