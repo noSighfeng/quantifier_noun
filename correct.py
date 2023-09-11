87626fc6dabaafad34cad652cff07e9b5b3f3ce3
noSighfeng <lzyf1019@qq.com>
2023-09-11 10:48:40 +0800

    增加正确句子输出

--- correct.py
+++ correct.py
@@ -10,7 +10,7 @@ class correct:
         self.num_list = num_list
         self.quantifier_noun_dict = quantifier_noun_dict
         self.pattern = ''
-    
+
     def init_pattern(self):
         # 数字 量词 名词
         self.pattern = ('([\d{}]+)' + '({})' + '(?:.*?)').format(''.join(self.num_list)
@@ -21,19 +21,25 @@ class correct:
         # 分词后根据定义的字典提取可能的名词
         words = [word for word,flag in posseg.lcut(sentence) if flag in ['n']]
         # 查找这些名词所搭配的[数词量词名词]
-        self.pattern = self.pattern + '([{}]+)'.format(''.join(words))
+        if len(words) != 0 : 
+            self.pattern = self.pattern + '({})'.format('|'.join(words))
         return re.finditer(self.pattern,sentence)
 
     def correct(self,sentence:str): # [('一', '篇', '话')]
-        if sentence ==' ' or len(sentence)==0: return 
+        if sentence ==' ' or len(sentence)==0: return
         mybe = self.find(sentence)
         # if len(mybe) == 0 : print('没找到数词量词名词搭配') # 没找到数词量词名词搭配
         errors_span_rights = [] # [(error,index,right)] # 错误列表error[('一', '篇', '话')]  错误范围span  过错后的词right
         sentence_right = sentence # 改错后的句子
         for i in mybe:
-            index1,index2,index3 = i.span(1),i.span(2),i.span(3)
+            try:
+                index1,index2,index3 = i.span(1),i.span(2),i.span(3)
+            except Exception:
+                print('句子不完整')
+                break
             span = i.span()
             i = list(i.groups())
+            #print(i)
             res = []
             try:
                 if i[2] not in self.quantifier_noun_dict[i[1]]: # 如果名词不在所搭配的量词词典中
@@ -41,47 +47,49 @@ class correct:
                     for k,v in self.quantifier_noun_dict.items(): # 遍历词典找到名词词可搭配的量词
                         if i[2] in v:
                             res.append(k)
-                    #print(i[0] + '[%s]' % '|'.join(res) + i[2],end='\t') 
-                    errors_span_rights.append((i,span,(i[0] , '[%s]' % '|'.join(res) , i[2]),(index1,index2,index3)))
+                    #print(i[0] + '[%s]' % '|'.join(res) + i[2],end='\t')
+                    errors_span_rights.append((i,span,(i[0] , '[%s]' % '|'.join(res) , i[2]),[index1,index2,index3]))
                     #print(span)
                     #print('正确句子：{}'.format(sentence.replace(''.join(i),i[0] + '[%s]' % '|'.join(res) + i[2])))
-                else: 
+                else:
                     # print('暂无错误 {}'.format(i))
                     print
             except Exception:
-            # 存在非量词
+                # 存在非量词
                 print('['+''.join(i) + ']句子中的[' + i[1] + ']不是量词')
                 for k,v in self.quantifier_noun_dict.items():
-                        if i[2] in v:
-                            res.append(k)
-                print(i[0] + '[%s]' % '|'.join(res) + i[2]) 
+                    if i[2] in v:
+                        res.append(k)
+                print(i[0] + '[%s]' % '|'.join(res) + i[2])
         if len(errors_span_rights) ==0:
             return
-        # sentence_right = list(sentence_right)
-        #last_len = len(sentence_right)
-        # for i in errors_span_rights:
-        #     sentence_right[i[3][1][0]+len(sentence_right) -  last_len:i[3][1][1]+len(sentence_right) -  last_len] = i[2][1]
-        #     last_len = len(sentence_right)
+        sentence_right = list(sentence_right)
+        for i in errors_span_rights:
+            sentence_right[i[3][1][0]] =  i[2][1]
 
-        print(errors_span_rights)
-        #print(''.join(sentence_right))
+        # print(errors_span_rights)
+        print('正确句子',''.join(sentence_right))
 
-quantifier_list = [] # 量词列表
-num_list = [] 
-noun_list = set() # 名词列表
-quantifier_noun_dict = {} # {'句' : ('话')}
+
+quantifier_list = []  # 量词列表
+num_list = []
+noun_list = set()  # 名词列表
+quantifier_noun_dict = {}  # {'句' : ('话')}
+
 
 # 加载数据
 def init_list():
 
-    num_path = os.path.join(os.getcwd(),'data_txt/num.txt')
-    with open(num_path,'r',encoding='utf-8') as f:
+
+    num_path = os.path.join(os.getcwd(), 'data_txt/num.txt')
+    with open(num_path, 'r', encoding='utf-8') as f:
         for line in f:
             num_list.append(line.strip())
 
     # 初始化量词列表，名词列表，量词-名词词典
-    quantifier_noun_dict_path = os.path.join(os.getcwd(),'data_txt/result.txt')
-    with open(quantifier_noun_dict_path,'r',encoding='utf-8') as f:
+    quantifier_noun_dict_path = os.path.join(os.getcwd(),
+                                             'data_txt/result.txt')
+    with open(quantifier_noun_dict_path, 'r', encoding='utf-8') as f:
         for line in f:
             line_spl = line.strip().split(' ')
             quan = line_spl[0]
@@ -92,9 +100,9 @@ def init_list():
                 noun_list.add(i)
             # 量词-名词词典
             quantifier_noun_dict[quan] = set(line_spl[1:])
-    
+
     # 加载第二份量词-名词词典
-    with open('data_txt/2_quan_noun.txt','r',encoding='utf-8') as f:
+    with open('data_txt/2_quan_noun.txt', 'r', encoding='utf-8') as f:
         for line in f:
             line_spl = line.strip().split(' ')
             quan = line_spl[0]
@@ -108,12 +116,9 @@ def init_list():
                 quantifier_noun_dict[quan] = set()
             quantifier_noun_dict[quan].update(line_spl[1:])
 
-    
-    
-
     # 初始化自定义词典
-    dict_custom_path = os.path.join(os.getcwd(),'data_txt/dict_custom.txt')
-    with open(dict_custom_path,'r',encoding='utf-8') as f:
+    dict_custom_path = os.path.join(os.getcwd(), 'data_txt/dict_custom.txt')
+    with open(dict_custom_path, 'r', encoding='utf-8') as f:
         for line in f:
             line_spl = line.strip().split(' ')
             quan = line_spl[0]
@@ -127,42 +132,30 @@ def init_list():
                 quantifier_noun_dict[quan] = set()
             quantifier_noun_dict[quan].add(line_spl[1])
 
-    
     # 名词列表写入jieba自定义词典
-    with open('data_txt/jieba_dict_custom.txt','w',encoding='utf-8') as f:
+    with open('data_txt/jieba_dict_custom.txt', 'w', encoding='utf-8') as f:
         for i in noun_list:
             f.write(i + ' 10 ' + 'n' + '\n')
 
     # 纠正错误词典 错误类型：量词错误搭配
-    dict_error_path = os.path.join(os.getcwd(),'data_txt/dict_error.txt')
-    with open(dict_error_path,'r',encoding='utf-8') as f:
+    dict_error_path = os.path.join(os.getcwd(), 'data_txt/dict_error.txt')
+    with open(dict_error_path, 'r', encoding='utf-8') as f:
         for line in f:
             line_spl = line.strip().split(' ')
-            error_quan , error_noun = line_spl[0] , line_spl[1]
+            error_quan, error_noun = line_spl[0], line_spl[1]
             quantifier_noun_dict[error_quan].discard(error_noun)
-            
-            
+
+    jieba.load_userdict("data_txt/jieba_dict_custom.txt")
+
+
 if __name__ == '__main__':
     init_list()
-    co = correct(noun_list,quantifier_list,num_list,quantifier_noun_dict)
-    text = [
-        '这篇文章的每一则话表达了作者这三种月以来的颠沛流离',
-        '表达了作者一个强烈的情感',
-        '一项情感',
-        '一各漂亮的苹果',
-        '每到中秋节，家家户户都会吃一轮圆圆的月饼',
-        '我的衣柜里有六件衣服，四件裤子和两件帽子,甚至两件鞋子',
-        '墙上挂着一副画',
-        '一对眼镜',
-        '一对马',
-        '她发现自己头上有一条白头发'
-    ]
+    co = correct(noun_list, quantifier_list, num_list, quantifier_noun_dict)
+    text = []
+    with open('test.txt', 'r',encoding='utf-8') as f:
+        for line in f:
+            text.append(line.strip())
     for i in text:
         co.correct(i)
 
-
-
-
-
-    
 
